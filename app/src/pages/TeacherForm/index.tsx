e49de5dda8551a84a5acd335c8f5e5f310e4b3ca
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-08-05 19:15:12 -0300

    create form

--- app/src/pages/TeacherForm/index.tsx
+++ app/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,63 @@
 import React from 'react'
+
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input'
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css'
 
 function TeacherForm(){
     return(
         <div id="page-teacher-form" className="container">
-        <PageHeader title="Que incrivel que você quer dar aulas." />
+        <PageHeader 
+          title="Que incrivel que você quer dar aulas." 
+          description="O primeiro passo é preenscher esse formmulário de inscrição"
+          />
+
+          <main>
+            <fieldset>
+              <legend>Seus daddos</legend>
+              
+              <Input name="name" label="Nome Completo"/>
+              <Input name="avatar" label="Avatar"/>
+              <Input name="whatsapp" label="Whatsapp"/>
+              <Textarea name="bio" label="Biografia"/>
+           </fieldset>
+
+           <fieldset>
+              <legend>Sobre a Aula</legend>
+              
+              <Select 
+                name="subject" 
+                label="Matéria"
+                options={[
+                  {value: 'Artes', label: 'Artes'},
+                  {value: 'Biologia', label: 'Biologia'},
+                  {value: 'Ciências', label: 'Ciências'},
+                  {value: 'Física', label: 'Física'},
+                  {value: 'Química', label: 'Artes'},
+                  {value: 'Geografia', label: 'Geografia'},
+                  {value: 'História', label: 'História'},
+                  {value: 'Matemática', label: 'Matemática'},
+                  {value: 'Português', label: 'Português'}
+                ]}
+                />
+              <Input name="cost" label="Custo da sua hora por aula"/>
+             
+           </fieldset>
+
+           <footer>
+             <p>
+               <img src={warningIcon} alt="Aviso importante"/>
+               Importante! <br />
+               Preencha todos os dados
+             </p>
+             <button type="button">Salvar Cadastro</button>
+           </footer>
+          </main>
       </div>
     )
 }
