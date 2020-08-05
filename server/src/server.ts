4e3e434e0870784236cfd0ec8a7ff2516c4a604e
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-08-04 22:40:05 -0300

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes'
 
 
 const app  = express();
 
+app.use(cors())
 app.use(express.json());
+app.use(routes);
 
-app.get("/", (request, response)=>{
-   return response.json({message: 'Hello World'});
-});
+
+
 
 
 app.listen(3333);
