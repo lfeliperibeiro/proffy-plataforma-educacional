54d8bd0c6142e507e91127171699f8a96f24ad55
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-08-05 23:24:02 -0300

    configure db relationship

--- app/src/components/TeacherItem/index.tsx
+++ app/src/components/TeacherItem/index.tsx
@@ -1,34 +1,49 @@
 import React from 'react';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './style.css'
 
-function TeacherItem(){
+export interface Teacher{    
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string    
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) =>{
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
     return(
         <article className="teacher-item">
         <header>
-            <img src="https://avatars1.githubusercontent.com/u/53883371?s=460&u=0a08333623e5c50ddf49c362a76133bd341ec710&v=4" alt="Felipe"/>   
+            <img src={teacher.avatar} alt={teacher.name}/>   
             <div>                    
-            <strong>Felipe Ribeiro</strong>
-            <span>Química</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
             </div> 
         </header> 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 
-                pessoas já passaram por uma das minhas explosões.
-             </p> 
+            <p>{teacher.bio}</p> 
 
         <footer>
             <p>Preço/hora
-                <strong>R$ 100,00</strong>
+                <strong>{teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a target="_blank" onClick={createNewConnection}  href={`https://wa.me/${teacher.whatsapp}`}>
                 <img src={whatsappIcon} alt="whatsapp"/>
                 Entrar em contato
-            </button>
+            </a>
         </footer>
     </article>   
     );
